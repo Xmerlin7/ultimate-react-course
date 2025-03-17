a23c0dd876ea23b9b9bda54e9d02896703f93c05
Seif Allah <saifeldarageely@gmail.com>
2025-03-18 00:12:38 +0200

    Add Pizza component to display pizza names in index.js

--- 03-pizza-menu/pizza/src/index.js
+++ 03-pizza-menu/pizza/src/index.js
@@ -6,11 +6,16 @@ const App = () => {
   return (
     <div>
       <h1>I did it, Im on the way</h1>
- 
+      {pizzaData.map(pia => <Pizza name={pia.name} />)}
     </div>
   );
 };
-
+function Pizza({name}){
+  return(
+    // <img src="" alt="-"></img>
+    <p>{name}</p>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
