eba49d608669bfde9c90f1a80485afdd4a9ec105
Seif Allah <saifeldarageely@gmail.com>
2025-03-17 03:37:32 +0200

    Update App component to display a message within a div for improved structure

--- 03-pizza-menu/pizza/src/index.js
+++ 03-pizza-menu/pizza/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function App() {
-  return <h1>Im coming</h1>;
-}
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+const App = () => {
+  return (
+    <div>
+      <h1>I did it, Im on the way</h1>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
