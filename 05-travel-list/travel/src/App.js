f2469b42e481c286dcecd4368319c85e9fba4ec6
Seif Allah <saifeldarageely@gmail.com>
2025-03-19 07:00:21 +0200

    Add main app structure with Logo, Form, PackingList, and Stats components

--- 05-travel-list/travel/src/App.js
+++ 05-travel-list/travel/src/App.js
@@ -1,6 +1,31 @@
-
 function App() {
-  return <div>hi</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
-
+function Logo() {
+  return <h1>🌴 Far Away 🛫</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>what do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list"></div>;
+}
+function Stats() {
+  return (
+    <footer>
+      you have X numbers on your list, and you already pcked X (%x)
+    </footer>
+  );
+}
 export default App;
