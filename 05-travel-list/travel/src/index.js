b2e06dfffbe836679986c808ffdc1dbb2bced7e0
Seif Allah <saifeldarageely@gmail.com>
2025-03-23 05:17:05 +0200

    Remove unused React imports and rendering code from index.js

--- 05-travel-list/travel/src/index.js
+++ 05-travel-list/travel/src/index.js
@@ -1,12 +1,4 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+
 // Destructing  Way
 
 // import React, { StrictMode } from "react";
