46c6767bb941139478925efa4b7b2a01ca90e66b
Seif Allah <saifeldarageely@gmail.com>
2025-05-05 17:06:36 +0300

    Refactor ListBox component by extracting MovieList for improved readability and maintainability

--- 07-usepopcorn/starter/usecorn/src/App.js
+++ 07-usepopcorn/starter/usecorn/src/App.js
@@ -103,7 +103,7 @@ function NavBar() {
   );
 }
 function ListBox() {
-  const [movies, setMovies] = useState(tempMovieData);
+  
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -113,26 +113,29 @@ function ListBox() {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && (
-        <ul className="list">
-          {movies?.map((movie) => (
-            <li key={movie.imdbID}>
-              <img src={movie.Poster} alt={`${movie.Title} poster`} />
-              <h3>{movie.Title}</h3>
-              <div>
-                <p>
-                  <span>🗓</span>
-                  <span>{movie.Year}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+      {isOpen1 && <MovieList />}
     </div>
   );
 }
-
+function MovieList() {
+  const [movies, setMovies] = useState(tempMovieData);
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <li key={movie.imdbID}>
+          <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          <h3>{movie.Title}</h3>
+          <div>
+            <p>
+              <span>🗓</span>
+              <span>{movie.Year}</span>
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
 function WatchedBox() {
   const [watched, setWatched] = useState(tempWatchedData);
   const [isOpen2, setIsOpen2] = useState(true);
