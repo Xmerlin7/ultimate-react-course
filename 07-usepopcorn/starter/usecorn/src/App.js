09b8abe9456d75249eee97150f432a50570780c0
Seif Allah <saifeldarageely@gmail.com>
2025-05-05 18:56:42 +0300

    Fix Result component to correctly display the number of found movies

--- 07-usepopcorn/starter/usecorn/src/App.js
+++ 07-usepopcorn/starter/usecorn/src/App.js
@@ -96,7 +96,7 @@ function Search() {
 function Result({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>movies.length</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
