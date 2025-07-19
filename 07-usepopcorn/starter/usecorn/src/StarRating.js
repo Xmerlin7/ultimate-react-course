f36d1c463054e66aba07e7335ae8009c4edc76a6
Seif Allah <saifeldarageely@gmail.com>
2025-07-19 19:21:02 +0300

    Fix typo in clickHandler function and improve star rendering logic in StarRating component

--- 07-usepopcorn/starter/usecorn/src/StarRating.js
+++ 07-usepopcorn/starter/usecorn/src/StarRating.js
@@ -7,7 +7,7 @@ const RatingStyle = {
 };
 const StarRating = ({ maxRating = 5 }) => {
   const [rating, setRating] = useState(0);
-  const clickHander = (newRating) => {
+  const clickHandler = (newRating) => {
     setRating(newRating);
   };
   return (
@@ -15,7 +15,7 @@ const StarRating = ({ maxRating = 5 }) => {
       <div>
         {Array.from({ length: maxRating }, (_, index) => (
           <span key={index}>
-            <Star onRating={() => clickHander(index + 1)} full={index + 1 <= rating} />
+            <Star onRating={() => {clickHandler(index + 1)}} full={rating >= index + 1} />
           </span>
         ))}
       </div>
@@ -24,7 +24,7 @@ const StarRating = ({ maxRating = 5 }) => {
   );
 };
 
-function Star({ onRating, full = false }) {
+function Star({ onRating, full }) {
   return !full ? (
     <svg
       onClick={onRating}
@@ -48,7 +48,7 @@ function Star({ onRating, full = false }) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
       fill="#FFD700"
-      stroke="#000"
+      stroke="#FFD700"
     >
       <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
     </svg>
