0569915615bd80a9b7b7f5072ebd76b802229ef6
Seif Allah <saifeldarageely@gmail.com>
2025-08-04 15:51:44 +0300

    Refactor List class to enhance method implementations and improve linked list operations

--- test.js
+++ test.js
@@ -29,6 +29,73 @@ class List {
       this.head = null;
       return;
     }
-    
+    let currentNode = this.head;
+    while (currentNode.next !== this.tail) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = null;
+    this.tail = currentNode;
+  }
+  shift() {
+    if (!this.head) {
+      console.log(" This Linked List is empty");
+      return;
+    }
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      return;
+    }
+    // CG will take care of it
+    this.head = this.head.next;
+  }
+  unshift(value) {
+    if (!this.head) {
+      this.push(value);
+      return;
+    }
+    let newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+  print(){
+    if (!this.head) {
+      console.log(" This Linked List is empty");
+      return;
+    }
+    let currentNode = this.head
+    let result = []
+    while(currentNode){
+      result.push(currentNode.value)
+      currentNode = currentNode.next
+      
+    }
+    console.log(`The current values of linked list are ${result.join(" -> ")}`);
+  }
+  size(){
+    if (!this.head) {
+      console.log(" This Linked List is empty");
+      return;
+    }
+    let currentNode = this.head
+    let ctr = 0;
+    while(currentNode){
+      ctr++
+      currentNode = currentNode.next
+    }
+    console.log(`The size of linked list is ${ctr}`)
   }
 }
+let list = new List()
+list.push(5)
+list.push(6)
+list.push(7)
+list.push(8)
+list.unshift(4)
+list.print()
+list.size()
+list.shift()
+list.print()
+list.pop()
+list.print()
+list.size()
